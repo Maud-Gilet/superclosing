21d8277938bdfbd03b0f2ddb8a2373c56d8cccba
HeleneLioussou <helene.lioussou@gmail.com>
2018-12-04 18:30:03 +0100

    evolution de la show de l'operation

--- config/routes.rb
+++ config/routes.rb
@@ -9,12 +9,12 @@ Rails.application.routes.draw do
 
   resources :companies do
    resources :s_documents, only: [:index, :show, :new, :create, :destroy]
-   resources :operations, only: [:index, :show, :new, :create] do
+   resources :operations, only: [:index, :new, :create] do
      resources :investments, only: [:index, :show, :new, :create, :edit, :update]
      resources :d_documents, only: [:index, :show]
    end
   end
-
+  resources :operations, only: :show
   resources :d_templates
   resources :roles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
