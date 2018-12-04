3321273c3ec26258d7a0b47fd0510616cc20e666
Mathieu Ribot <mribot@hotmail.fr>
2018-12-04 12:28:03 +0100

    Add dotenv - Protect my secret data in .env file

--- Gemfile
+++ Gemfile
@@ -19,9 +19,11 @@ gem 'webpacker'
 gem 'money-rails'
 gem 'cloudinary', '~> 1.9.1'
 gem 'carrierwave', '~> 1.2'
+gem 'postmark-rails'
 
 group :development do
   gem 'web-console', '>= 3.3.0'
+  gem "letter_opener"
 end
 
 group :development, :test do
--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,9 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+config.action_mailer.delivery_method = :postmark
+config.action_mailer.postmark_settings = { :api_token =>  }
+
 module Lmd
   class Application < Rails::Application
     config.generators do |generate|
