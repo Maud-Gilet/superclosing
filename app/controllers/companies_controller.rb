299c30d7ca4b3657cee591cd9386e84624879a74
Maud Gilet <maud.gilet@gmail.com>
2018-12-04 13:53:44 +0100

    operations controller coded

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,2 +1,6 @@
 class CompaniesController < ApplicationController
+
+  def show
+    @company = company.current_user
+  end
 end
--- app/controllers/operations_controller.rb
+++ app/controllers/operations_controller.rb
@@ -1,2 +1,29 @@
 class OperationsController < ApplicationController
+  def index
+    @company = Company.find(params[:company_id])
+    @operations = Operation.all.where(company: @company)
+  end
+
+  def new
+    @company = Company.find(params[:company_id])
+    @operation = Operation.new
+  end
+
+  def create
+    @operation = Operation.new(operation_params)
+    @operation.company = Company.find(params[:company_id])
+    @operation.status = "Pending"
+    @operation.save!
+    redirect_to company_operation_path(params[:company_id], @operation.id)
+  end
+
+  def show
+    @operation = Operation.find(params[:id])
+  end
+
+  private
+
+  def operation_params
+    params.require(:operation).permit(:name, :category, :target_amount_cents, :expected_closing_date)
+  end
 end
