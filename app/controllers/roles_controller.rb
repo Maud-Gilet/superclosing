70727f180a8e8a11cc07e7f83bd386f5affab605
HeleneLioussou <helene.lioussou@gmail.com>
2018-12-04 15:14:07 +0100

    controller de roles

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -1,2 +1,24 @@
 class RolesController < ApplicationController
+
+  def index
+    @roles = Role.all
+  end
+
+  def new
+    @role = Role.new
+  end
+
+  def create
+    @role = Role.new(role_params)
+    @role.save
+
+    redirect_to company_path(company)
+  end
+
+  private
+
+  def role_params
+    params.require(:role).permit(:category, :user_id, :company_id)
+  end
+
 end
