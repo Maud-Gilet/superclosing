1223e04d033a5fc64c2a216c5093e200cc2274fe
Maud Gilet <maud.gilet@gmail.com>
2018-12-05 18:58:10 +0100

    c est bon

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,5 +7,6 @@ class PagesController < ApplicationController
   def home
       @associe = current_user.roles.where(category: 'Associe')
       @investisseur = current_user.roles.where(category: 'Investisseur')
+      @company = Company.new
   end
 end
--- app/javascript/components/init_autocomplete_company.js
+++ app/javascript/components/init_autocomplete_company.js
@@ -1,9 +1,8 @@
-const inputSiren = document.getElementById('company-siren-field');
-const inputName = document.getElementById('company-name-field');
-const inputAddress = document.getElementById('company-address-field');
-const inputLegalForm = document.getElementById('company-legal-form-field');
-const inputCreationDate = document.getElementById('company-creation-date-field');
-const inputFiscalDate = document.getElementById('company-fiscal-date-field');
+const inputSirenForm = document.getElementById('company_siren');
+const inputName = document.getElementById('company_name');
+const inputAddress = document.getElementById('company_address');
+const inputLegalForm = document.getElementById('company_legal_form');
+const inputCreationDate = document.getElementById('company_creation_date');
 
 const drawResponse = (data) => {
 
@@ -19,7 +18,7 @@ const drawResponse = (data) => {
 };
 
 const autocomplete = (event) => {
-  const sirenQuery = inputSiren.value;
+  const sirenQuery = event.currentTarget.value;
   const url = `https://entreprise.data.gouv.fr/api/sirene/v1/siren/${sirenQuery}`;
   fetch(url, {
     headers: {'Authorization': 'Bearer bfd874df-3924-3396-b372-541e25bbfbd9'}
@@ -29,7 +28,7 @@ const autocomplete = (event) => {
 };
 
 const autocompleteApiSirene = () => {
-  inputSiren.addEventListener('change', autocomplete);
+  inputSirenForm.addEventListener('focusout', autocomplete);
 };
 
 export { autocompleteApiSirene };
