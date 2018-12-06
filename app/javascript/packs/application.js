4384982ac221b20fa5bb6d1cf8ac4f945a6747f4
HeleneLioussou <helene.lioussou@gmail.com>
2018-12-06 14:32:43 +0100

    premier schema

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,5 +3,16 @@ import "bootstrap";
 import { displayOperationForm } from '../components/operation_creation_form';
 import { autocompleteApiSirene } from '../components/init_autocomplete_company';
 
+import { graph } from '../components/init_operation_graph'
+
+if(document.getElementById('myChart')){
+ graph();
+}
+if(document.getElementById("operation_category")){
 displayOperationForm();
-autocompleteApiSirene();
+}
+if(document.getElementById('company_siren')){
+ autocompleteApiSirene();
+}
+
+
