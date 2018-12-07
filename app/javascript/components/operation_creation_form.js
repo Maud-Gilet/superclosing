a3a072a50c8b6351f5c17b7b8d44cd3ac6028c63
Maud Gilet <maud.gilet@gmail.com>
2018-12-07 15:43:45 +0100

    show updated

--- app/javascript/components/operation_creation_form.js
+++ app/javascript/components/operation_creation_form.js
@@ -7,8 +7,8 @@ const displayOperationForm = (data) => {
       formField.insertAdjacentHTML('afterend', `<div class="buttons-new-operation"><button type="submit" class="btn btn-create-operation">Créer</button><button type="button" class="btn btn-cancel-operation">Annuler</button></div>`)
       formField.insertAdjacentHTML('afterend', `<div> <label class="control-label" for="date">Date de clôture envisagée</label>
         <input class="form-control" id="date" name="operation[expected_closing_date]" placeholder="JJ/MM/AAAA" type="text"/> </div>`)
-      formField.insertAdjacentHTML('afterend',`<div class="form-group integer optional operation_target_amount form-group-valid"><label class="form-control-label integer optional" for="operation_target_amount">Montant visé ? (€) </label><input class="form-control is-valid numeric integer optional" type="number" step="1000" value="0" name="operation[target_amount]" id="operation_target_amount"></div>`);
-    formField.insertAdjacentHTML('afterend',`<div class="form-group integer optional operation_premoney form-group-valid"><label class="form-control-label integer optional" for="operation_premoney">Valorisation pre-money? (€) </label><input class="form-control is-valid numeric integer optional" type="number" step="1000" value="0" name="operation[premoney]" id="operation_premoney"></div>`);
+      formField.insertAdjacentHTML('afterend',`<div class="form-group integer optional operation_target_amount form-group-valid"><label class="form-control-label integer optional" for="operation_target_amount">Montant de la levée (€) </label><input class="form-control is-valid numeric integer optional" type="number" step="1000" value="0" name="operation[target_amount]" id="operation_target_amount"></div>`);
+    formField.insertAdjacentHTML('afterend',`<div class="form-group integer optional operation_premoney form-group-valid"><label class="form-control-label integer optional" for="operation_premoney">A combien votre société est-elle valorisée ? (€) </label><input class="form-control is-valid numeric integer optional" type="number" step="1000" value="0" name="operation[premoney]" id="operation_premoney"></div>`);
   };
   });
 };
