d13f7f76a44aae126bfd8ffed1d9f80c6087bbe0
Mathieu Ribot <mribot@hotmail.fr>
2018-12-08 11:49:59 +0100

    flatpickr is working!

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,11 +1,37 @@
 import "bootstrap";
 
+// Datepicker for form (import from Node package)
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+// Autofilling fields (adding a company) when typing SIREN number of this company
 import { autocompleteApiSirene } from '../components/init_autocomplete_company';
+// Display subform when adding new operation
 import { displayOperationForm } from '../components/operation_creation_form';
 
+// Display graphs
 import { graph } from '../components/init_operation_graph'
 import { graphsecond } from '../components/init_operation_secondgraph'
+
+// Dynamic text for landing page banner
 import { loadDynamicBannerText } from '../components/typed_landing';
+
+
+// Functions calls
+
+if(document.querySelector('.datepicker')) {
+  flatpickr(".datepicker", {
+
+  });
+}
+
+if(document.getElementById('company_siren')){
+ autocompleteApiSirene();
+}
+
+if(document.getElementById("operation_category")){
+  displayOperationForm();
+}
 
 if(document.getElementById('myChart')){
  graph();
@@ -13,13 +39,7 @@ if(document.getElementById('myChart')){
 if(document.getElementById('mySecondChart')){
  graphsecond();
 }
-if(document.getElementById("operation_category")){
-displayOperationForm();
-}
-if(document.getElementById('company_siren')){
- autocompleteApiSirene();
-}
 
 if(document.getElementById('typed_text')){
-loadDynamicBannerText();
+  loadDynamicBannerText();
 }
