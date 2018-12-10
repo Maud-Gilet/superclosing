791b88612993fb40c3879bfbab85b57f4cfa9868
Maud Gilet <maud.gilet@gmail.com>
2018-12-10 18:28:25 +0100

    fix cd document

--- app/models/d_document.rb
+++ app/models/d_document.rb
@@ -3,9 +3,8 @@ class DDocument < ApplicationRecord
   belongs_to :d_template
   belongs_to :user, optional: true
 
-  validates :user, presence: true, #if: :d_document_is_a_subscription_bund?
+  validates :user, presence: true #if: :d_document_is_a_subscription_bund?
 
-  private
 
   # def d_document_is_a_subscription_bund?
   #   document_type == "subscription_bund"
