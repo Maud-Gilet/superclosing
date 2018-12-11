7f223e2329db4d21324e18ed03c577193f40742c
Mathieu Ribot <mribot@hotmail.fr>
2018-12-11 16:38:03 +0100

    debug

--- app/controllers/operations_controller.rb
+++ app/controllers/operations_controller.rb
@@ -24,7 +24,6 @@ class OperationsController < ApplicationController
   def show
     @operation = Operation.find(params[:id])
     @s_document = SDocument.new
-
     @d_document = DDocument.new
 
     @shares_values = 0
